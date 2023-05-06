18535cd6e9709ff3ba8d335bb710fadba0728cd6
juandsoto <juandsoto2001@hotmail.com>
2023-05-06 09:33:50 -0500

    layout: Material icons added

--- client/src/pages/AdminPage.tsx
+++ client/src/pages/AdminPage.tsx
@@ -1,4 +1,6 @@
+import { ArticleOutlined, EditNoteOutlined, PersonAddOutlined, PersonOutline } from '@mui/icons-material';
 import { Logo } from "components";
+import React from "react";
 import { Link } from "react-router-dom";
 
 function AdminPage() {
@@ -28,30 +30,51 @@ function Sidebar() {
 			</Link>
 			<div className="mt-6 px-2 flex flex-col gap-4">
 				<div>
-					<span className="inline-block pb-1 uppercase text-sm italic font-semibold">Usuarios</span>
-					<ul>
-						<li className="hover:text-primary transition-colors w-fit">
-							<Link to='./users'>Lista de usuarios</Link>
-						</li>
-						<li className="hover:text-primary transition-colors w-fit">
-							<Link to='./users/create'>Crear un usuario</Link>
-						</li>
+					<span className="inline-block pb-1 uppercase text-sm italic font-semibold">Users</span>
+					<ul className="leading-6">
+						<SidebarLink to='./users' icon={ <PersonOutline fontSize="inherit" /> } text="Users List" />
+						<SidebarLink to='./users/create' icon={ <PersonAddOutlined fontSize="inherit" /> } text="Create an user" />
 					</ul>
 				</div>
 				<hr />
 				<div>
-					<span className="inline-block pb-1 uppercase text-sm italic font-semibold">Hoteles</span>
-					<ul>
-						<li className="hover:text-primary transition-colors w-fit">
-							<Link to='./users'>Lista de hoteles</Link>
-						</li>
-						<li className="hover:text-primary transition-colors w-fit">
-							<Link to='./users/create'>Crear un hotel</Link>
-						</li>
+					<span className="inline-block pb-1 uppercase text-sm italic font-semibold">News</span>
+					<ul className="leading-6">
+						<SidebarLink to='./news' icon={ <ArticleOutlined fontSize="inherit" /> } text="News List" />
+						<SidebarLink to='./news/create' icon={ <EditNoteOutlined fontSize="inherit" /> } text="Create news" />
 					</ul>
 				</div>
 			</div>
+			<div className="mt-6">
+				<span className="inline-block text-lg text-primary font-semibold ml-2 mb-1">Reports</span>
+				<ul className="space-y-1">
+					<Link to='./report-one'>
+						<li className="hover:bg-primary/40 transition-colors rounded-md p-2">
+							Report one
+						</li>
+					</Link>
+					<Link to='./report-two'>
+						<li className="hover:bg-primary/40 transition-colors rounded-md p-2">
+							Report two
+						</li>
+					</Link>
+				</ul>
+			</div>
 		</aside>
+	);
+}
+
+interface SidebarLinkProps {
+	to: string;
+	icon: React.ReactNode;
+	text: string;
+}
+
+function SidebarLink({ to, icon, text }: SidebarLinkProps) {
+	return (
+		<li className="hover:text-primary transition-colors w-fit">
+			<Link to={ to } className='flex items-center gap-1'>{ icon }<span>{ text }</span></Link>
+		</li>
 	);
 }
 
