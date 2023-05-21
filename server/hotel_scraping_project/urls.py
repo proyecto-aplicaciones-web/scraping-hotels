34bb249463d319c28c6f68dcfcf9a67c4df8ef19
winandtu <jyfy121@gmail.com>
2023-05-20 23:37:17 -0500

    Organize URLS

--- server/hotel_scraping_project/urls.py
+++ server/hotel_scraping_project/urls.py
@@ -14,23 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+"""from django.contrib import admin
 from django.urls import path
-from user_app import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    # User
-    path('create_user/', views.create_user, name='create_user'),
-    path('get_users/', views.get_users, name='get_users'),
-    path('get_user/<int:user_id>/', views.get_user, name='get_user'),
-    path('delete_user/<int:user_id>/', views.modify_user, name='delete_user'),
-    #path('modify_user/<int:user_id>/', views.delete_user, name='delete_user'),
-
-    #News
-    path('news/', views.NewsList.as_view(), name='NewsList'),
-    path('news/<int:news_id>', views.NewsId.as_view(), name='NewsId'),
-    path('news/create', views.NewsCreate.as_view(), name='NewsCreate'),
-    path('news/delete/<int:news_id>', views.NewsDelete.as_view(), name='NewsDelete'),
-
-]
+from user_app import views"""
+from user_app.urls import urlpatterns
