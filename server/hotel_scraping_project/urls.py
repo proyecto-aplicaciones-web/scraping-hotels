e3f3c0eb3e7fcc2f6117a1eb84c1bb3ef580596b
winandtu <jyfy121@gmail.com>
2023-05-20 23:34:42 -0500

    CRUDS of the News Finished

--- server/hotel_scraping_project/urls.py
+++ server/hotel_scraping_project/urls.py
@@ -26,4 +26,11 @@ urlpatterns = [
     path('get_user/<int:user_id>/', views.get_user, name='get_user'),
     path('delete_user/<int:user_id>/', views.modify_user, name='delete_user'),
     #path('modify_user/<int:user_id>/', views.delete_user, name='delete_user'),
+
+    #News
+    path('news/', views.NewsList.as_view(), name='NewsList'),
+    path('news/<int:news_id>', views.NewsId.as_view(), name='NewsId'),
+    path('news/create', views.NewsCreate.as_view(), name='NewsCreate'),
+    path('news/delete/<int:news_id>', views.NewsDelete.as_view(), name='NewsDelete'),
+
 ]
--- server/user_app/views.py
+++ server/user_app/views.py
@@ -1,12 +1,32 @@
 # Create your views here.
-
+#Django
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt 
+import json
+from django.utils.decorators import method_decorator
+from django.db.utils import IntegrityError
+from django.utils import timezone
+from django.contrib.auth import authenticate
+from django.db.models.functions import Lower
+from django.http import JsonResponse
+from django.http import Http404
+from django.shortcuts import render
+#Restframework
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.request import Request
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import serializers
+from rest_framework import status, generics
+from rest_framework import authentication, permissions
+from rest_framework.exceptions import NotFound
+#locals
 from .models import *
 from .serializers import *
+
 
 # User
 
@@ -47,3 +67,79 @@ def modify_user(request, user_id):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+#CRUDS NEWS
+
+#CREATE
+#@method_decorator(csrf_exempt, name='dispatch')
+class NewsCreate(generics.CreateAPIView):
+    permission_classes = [permissions.AllowAny]
+
+    # create a method POST with a try except for the use of fixs.
+    def post(self, request):
+        # Create an object of the class NewsSerializer, using the request.data
+        serializer = NewsSerializer(data=request.data)
+        # If the serializer is True 
+        if serializer.is_valid():
+            # Save the serializer 
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# Class List
+class NewsList(generics.ListAPIView):
+    serializer_class = NewsSerializer
+    model = News
+    permission_classes = [permissions.AllowAny]
+    queryset = News.objects.all()
+
+#Search for id
+
+@method_decorator(csrf_exempt, name='dispatch')
+class NewsId(generics.GenericAPIView):
+
+    serializer_class = NewsSerializer
+    model = News
+    permission_classes = [permissions.AllowAny]
+    queryset = News.objects.all()
+
+    def getNews(self, news_id):
+        try:
+            return News.objects.get(news_id=news_id)
+        except News.DoesNotExist:
+            raise Http404("The new not exist")
+
+    def get(self, request: Response, news_id=''):
+
+        new = self.getNews(news_id)
+        serializer = NewsSerializer(new, many=False)
+        return Response(data=serializer.data)
+
+
+
+#DELETE
+
+# Delete User with id
+
+class NewsDelete(generics.GenericAPIView):
+
+    serializer_class = NewsSerializer
+    model = News
+    permission_classes = [permissions.AllowAny]
+    queryset = News.objects.all()
+
+    def getNews(self, news_id):
+        try:
+            return News.objects.get(news_id=news_id)
+        except News.DoesNotExist:
+            raise Http404("No exist New")
+
+    def delete(self, request, news_id='', format=None):
+        news = self.getNews(news_id)
+
+        if news.delete():
+            return Response(status=status.HTTP_200_OK, data={"Delete"})
+        return Response(status=status.HTTP_204_NO_CONTENT)
