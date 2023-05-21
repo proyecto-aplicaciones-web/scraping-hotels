3b7e07b1b246eaeaa05d7054735f7f8681902cf1
William-Ballesteros-Cortes <65502699+William-Ballesteros-Cortes@users.noreply.github.com>
2023-05-21 15:05:20 -0500

    CAMBIO DE VIEWS Y URLS (USUARIO) Se cambio las urls y se completa las funciones de CRUD de usuarios COMPLETE

--- server/user_app/urls.py
+++ server/user_app/urls.py
@@ -5,9 +5,9 @@ from user_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     # User
-    path('user/create/', views.create_user, name='create_user'),
+    path('user/create', views.create_user, name='create_user'),
     path('users/get/all', views.get_users, name='get_users'),
-    path('users/get/<int:user_id>/', views.get_user, name='get_user'),
-    path('users/modify/<int:user_id>/', views.modify_user, name='modify_user'),
-    path('users/delete/<int:user_id>/', views.delete_user, name='delete_user'),
+    path('users/get/<int:user_id>', views.get_user, name='get_user'),
+    path('users/modify/<int:user_id>', views.modify_user, name='modify_user'),
+    path('users/delete/<int:user_id>', views.delete_user, name='delete_user'),
 ]
