012f7cd49b365625e2de3af5af739bbfb8641a08
juandsoto <juandsoto2001@hotmail.com>
2023-06-01 11:40:28 -0500

    feat: added google map

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -1,5 +1,6 @@
 const CONFIG = {
 	GOOGLE_RECAPTCHA_SECRET_KEY: import.meta.env.VITE_GOOGLE_RECAPTCHA_SECRET_KEY as string,
+	GOOGLE_MAPS_KEY: import.meta.env.VITE_GOOGLE_MAPS_KEY as string,
 	API_BASE_URL: import.meta.env.VITE_API_BASE_URL as string,
 }
 
