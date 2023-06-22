a06b2b44b897b8408e1d7b695906ac35b5316ada
Diego Gil <juan.gil.trujillo@correounivalle.edu.co>
2023-06-22 12:03:32 -0500

    achieve: SkyscannerSpider, BUT it's not implemented make catchap in that page, so this Spider doesn't work still

--- server/scraping_rooms/spiders/SkyscannerSpider.py
+++ server/scraping_rooms/spiders/SkyscannerSpider.py
@@ -1,6 +1,75 @@
-"""
-- tripadvisor https://www.tripadvisor.co/Hotels-g34438-oa90-Miami_Florida-Hotels.html 
-- booking https://www.booking.com/searchresults.es.html?label=gog235jc-1DCAMYAijsATidA0gKWANoMogBAZgBCrgBF8gBD9gBA-gBAfgBAogCAagCA7gCucCvowbAAgHSAiRlOTgzYzE2NC01ZTIwLTQxZWYtYmNmYS00M2JkNTFjNmZkNzbYAgTgAgE&sid=4c70767c1d5dc0ce44dbc7c280da7b3f&aid=356980&city=20023181&nflt=sth%253D7&pptfromtl=7&src=theme_landing_city&offset=25
-- kayak https://www.kayak.com.co/hotels/Miami,Florida,Estados-Unidos-c14305/2023-05-23/2023-05-24/2adults?sort=rank_a
-- skyscanner https://www.espanol.skyscanner.com/hotels/search?entity_id=27536644&checkin=2023-05-29&checkout=2023-05-30&adults=2&rooms=1&impression_id=440d1f4e-08be-4679-bcb0-6701d53042a8
-"""
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.selector import Selector
+from scrapy.loader import ItemLoader
+from itemloaders.processors import MapCompose 
+from scrapy.linkextractors import LinkExtractor
+from scraping_rooms.items import HotelRoomItem
+from datetime import datetime, timedelta
+
+def get_date_days_after(days:int)->str:
+    current_date = datetime.now()
+    date_after = current_date + timedelta(days=days)
+    return date_after.strftime('%Y-%m-%d')
+ 
+class SkyscannerSpider(CrawlSpider):
+    name = "SkyscannerSpider"
+    allowed_domains = ['espanol.skyscanner.com']
+    # start_urls = [f"https://www.espanol.skyscanner.com/hotels/search?entity_id=27539515&checkin={get_date_days_after(3)}&checkout={get_date_days_after(5)}&adults=2&rooms=1"]
+    start_urls = ["https://www.espanol.skyscanner.com/hotels/search?entity_id=27539515&","https://www.espanol.skyscanner.com/hoteles/colombia/cartagena-de-indias-hoteles/hotel-bantu-by-faranda-boutique%2c-a-member-of-radisson-individuals/ht-46989106?checkin=2023-06-29&checkout=2023-06-30&rooms=1&adults=2&clicked_details_price=532486&currency=COP&impression_id=da69afe6-5b70-4438-a2fc-390b105c3198&locale=es-MX&market=CO&poiId=104128815&priceType=price-per-night&search_cycle_id=2364ec470ec5709b1f4d96182f745fffdc22b166c7a895e6e22f2bdcb70840d3&search_entity_id=27540568" ]
+    # start_urls = ["https://www.espanol.skyscanner.com/hotels/"]
+     
+    download_delay = 5 #!
+    
+    custom_settings = {
+        'USER_AGENT': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+        # 'CLOSESPIDER_PAGECOUNT': 8, #!
+        'CLOSESPIDER_ITEMCOUNT': 2, #!
+        'DOWNLOAD_DELAY': 5,
+    }
+    
+    rules = (
+        Rule(
+            LinkExtractor(
+                allow=r'/hotels/search'
+            ), follow=True
+        ),
+        Rule(
+            LinkExtractor( 
+                allow=r'/hoteles/([^/]+)/'
+            ), follow=True, callback='parser_hotel_room' 
+        ), 
+    )
+    
+    def clean_price(self, price:str):
+       new_price = price.replace('$','').replace('.','').replace(' ','').replace(',','').strip()
+       if new_price:
+           return float(new_price)
+       else:
+           return -1.0
+       
+    def clean_score(self, price:str):
+       new_price = price.replace(',','.').replace(' ','').strip()
+       if new_price:
+           return round(float(new_price)/2, 1)
+       else:
+           return -1.0
+   
+    def parser_hotel_room(self, response):
+        sel = Selector(response)
+        item = ItemLoader(HotelRoomItem(), sel)
+        item.add_xpath('name','//*[@id="hotel-description"]/p//text()') 
+        item.add_xpath('description', '//*[@id="hotel-description"]/div/p//text()')  
+        item.add_xpath('price', '//*[@data-test-id="best-offer-price"]//text()', MapCompose(self.clean_price))  
+        item.add_xpath('score','//div[@data-test-id="reviews-ratings"]//div[@role="figure"]/span[@aria-hidden="true"][1]//text()', MapCompose(self.clean_score)) 
+        item.add_xpath('geolocation','//button[@data-test-id="address-button"]//text()') 
+        item.add_value('link', [response.url]) 
+        item.add_xpath('discount', '//*[@data-test-id="hotel-card-badge"]//text()')
+        
+        item.add_xpath('services', '//*[@id="hotel-details-amenities-section"]//span/following-sibling::p//text()')
+        yield item.load_item()
+    
+    
+    
+    
+        
+        
